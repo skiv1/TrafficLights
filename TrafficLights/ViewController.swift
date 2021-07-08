2d7f73294d98fa99459c608ebf3694709e4b5563
Anton Grekov <grekov_anton@bk.ru>
2021-07-08 08:39:34 +0300

    квадраты в круги

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,10 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      let sizeForCircle = redLight.frame.size.width / 2
+        redLight.layer.cornerRadius =  sizeForCircle
+        yellowLight.layer.cornerRadius = sizeForCircle
+        greenLight.layer.cornerRadius = sizeForCircle
+        
     }
 
 
